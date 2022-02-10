c4d53ad661e54f5e6c1df4f199936f12ead1959a
Paul <pkilgarriff@gmail.com>
2022-02-10 11:54:56 +0000

    Refactor student count output to use variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # First print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # Finally print the total number of students
-print "Overall, we have "
-# Using print to avoid newline characters
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students"
