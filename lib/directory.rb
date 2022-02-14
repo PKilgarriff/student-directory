778b2b1b8758e945883f7782192d81f325f903a1
Paul <pkilgarriff@gmail.com>
2022-02-14 15:38:00 +0000

    Create multiple methods for interactive_menu - print_menu - show_students -process(selection)

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,31 +1,43 @@
 require_relative 'constants'
 
 # Allows for passing default argument to use prepopulated array of student hashes
-$default_students = ARGV.include?('default') ? true : false
+@default_students = ARGV.include?('default') ? true : false
 ARGV.clear
 
+def print_menu
+  # 1. Print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit program"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit # Terminates program
+  else
+    puts "Invalid selection"
+  end
+end
+
 def interactive_menu
-  students = $default_students ? STUDENTS : []
+  @students = @default_students ? STUDENTS : []
   loop do
-    # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit program"
+    print_menu
     # 2. Read the input and save it as a variable
     selection = gets.chomp
     # 3. Carry out the task the user has asked for
-    case selection
-    when '1'
-      input_students
-    when '2'
-      print_header
-      print(students)
-      print_footer(students)
-    when '9'
-      exit # Terminates program
-    else
-      puts "Invalid selection"
-    end
+    process(selection)
   end
 end
 
@@ -34,8 +46,6 @@ def input_students
   puts 'Please enter student information'
   puts 'Enter the student name, cohort, and their place of birth, separated by commas'
   puts 'To finish, just hit return twice'
-  # create an empty array
-  students = []
   name = 'To be Overwritten'
 
   # get the first name
@@ -48,24 +58,22 @@ def input_students
     birthplace = "Unknown" if birthplace === nil
     puts "Name: #{name}, Cohort: #{cohort}, Birthplace: #{birthplace}"
     # add the student hash to the array
-    students << { name: name, cohort: cohort.downcase.to_sym, birthplace: birthplace }
-    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
+    @students << { name: name, cohort: cohort.downcase.to_sym, birthplace: birthplace }
+    puts "Now we have #{@students.count} #{@students.count == 1 ? "student" : "students"}"
   end
-  # return the array of students
-  students
 end
 
 # Return an array of the cohorts in this particular array of hashes
-def cohorts(students)
-  students.map { |student| student[:cohort] }.uniq
+def cohorts
+  @students.map { |student| student[:cohort] }.uniq
 end
 
 # Print the student list grouped by cohort
-def print_by_cohort(students)
-  cohorts = cohorts(students)
+def print_by_cohort
+  # cohorts = cohorts
   cohorts.each do |cohort|
     puts cohort.capitalize
-    current_cohort = students.select { |student| student[:cohort] == cohort }
+    current_cohort = @students.select { |student| student[:cohort] == cohort }
     current_cohort.each { |student| puts "\t#{student[:name]}" }
   end
 end
@@ -79,42 +87,29 @@ end
 
 # Takes an array as an input and 'puts' each value within
 # def print(students, starting_with)
-def print(students)
-  if students.count == 0
+def print
+  if @students.count == 0
     puts 'No students are currently enrolled'
     return
   end
-  longest_name = students.map { |student| student[:name] }.max_by(&:length)
-  longest_birthplace = students.map { |student| student[:birthplace] }.max_by(&:length)
-  students.each_with_index do |student, idx|
-    # next unless student[:name].length < 12
-
-    # if student[:name][0].downcase == starting_with.downcase
+  longest_name = @students.map { |student| student[:name] }.max_by(&:length)
+  longest_birthplace = @students.map { |student| student[:birthplace] }.max_by(&:length)
+  @students.each_with_index do |student, idx|
     student_output = "#{(idx + 1).to_s.ljust(2)} | "\
     "#{student[:name].center(longest_name.length)} | "\
     "#{student[:birthplace].center(longest_birthplace.length)} | "\
     "#{student[:cohort].capitalize}"
     puts student_output
   end
-  # idx = 0
-  # while idx < students.length
-  #   student_output = "#{(idx + 1).to_s.ljust(2)} | "\
-  #   "#{students[idx][:name].center(longest_name.length)} | "\
-  #   "#{students[idx][:birthplace].center(longest_birthplace.length)} | "\
-  #   "#{students[idx][:cohort].capitalize}"
-  #   puts student_output
-  #   idx += 1
-  # end
 end
 
 # Puts a footer line containing a count of the passed in argument
-def print_footer(students)
-  return if students.count == 0
+def print_footer
+  return if @students.count == 0
 
-  puts "Overall, we have #{students.count} #{ADJECTIVES.sample} #{students.count == 1 ? "student" : "students"}"
+  puts "Overall, we have #{@students.count} #{ADJECTIVES.sample} #{@students.count == 1 ? "student" : "students"}"
 end
 
-
-puts $default_students
+puts @default_students
 # Call interactive_menu to run other methods based on user input
 interactive_menu
